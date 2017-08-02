a5d896755032a9f777ea135b0adab014226430c1
BoscoMurray <rossamurray@gmail.com>
2017-08-02 18:17:27 +0100

    can get beers from API

--- public/index.js
+++ public/index.js
@@ -1,4 +1,23 @@
+var makeRequest = function(url, callback) {
+  var request = new XMLHttpRequest();
+
+  request.addEventListener('load', callback);
+
+  request.open('GET', url);
+  request.send();
+}
+
+var beers;
+
+var requestComplete = function() {
+  if (this.status !== 200) return;
+  var jsonString = this.responseText;
+  beers = JSON.parse(jsonString);
+}
+
 var app = function () {
+  var url = 'https://api.punkapi.com/v2/beers';
+  makeRequest(url, requestComplete);
 
 }
 
