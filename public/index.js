024c9e0204cd43955fe60472d968c4e5a8f88293
BoscoMurray <rossamurray@gmail.com>
2017-08-02 18:39:39 +0100

    can list beers on page

--- public/index.js
+++ public/index.js
@@ -13,6 +13,20 @@ var requestComplete = function() {
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   beers = JSON.parse(jsonString);
+  populateList(beers);
+}
+
+var createLi = function(beer) {
+  var ul = document.querySelector('#beer-list');
+  var li = document.createElement('li');
+  li.innerText = beer.name;
+  ul.appendChild(li);
+}
+
+var populateList = function(beers) {
+  // var ul = document.querySelector('#beer-list');
+  console.log(beers);
+  beers.forEach(createLi);
 }
 
 var app = function () {
